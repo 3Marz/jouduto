d558ffd548f6713bc89e607b6a99fb30263cb17f
3Marz <omar490v@gmail.com>
2026-08-23 23:29:25 +0300

    changed db schema

--- constants.py
+++ constants.py
@@ -17,7 +17,6 @@ CREATE TABLE IF NOT EXISTS items (
     item_id INTEGER PRIMARY KEY,
     code TEXT NOT NULL UNIQUE,
     name TEXT NOT NULL,
-    unit_price NUMERIC NOT NULL DEFAULT 0.00,
     created_at TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -32,15 +31,14 @@ CREATE TABLE IF NOT EXISTS item_distributors (
 );
 
 CREATE TABLE IF NOT EXISTS inventory (
-    warehouse_id INTEGER NOT NULL,
-    item_id INTEGER NOT NULL,
+    inventory_id INTEGER PRIMARY KEY,
+    item_id INTEGER NOT NULL UNIQUE,
     quantity_available INTEGER DEFAULT 0,
     quantity_ordered INTEGER DEFAULT 0,
     quantity_sold INTEGER DEFAULT 0,
+    cost_price NUMERIC DEFAULT 0.0,
     last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (warehouse_id, item_id),
-    FOREIGN KEY (item_id) REFERENCES items(item_id),
-    FOREIGN KEY (warehouse_id) REFERENCES warehouses(warehouse_id)
+    FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
 """
 
